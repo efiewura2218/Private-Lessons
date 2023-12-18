fdc7109e0aa71aa740fb756a264124e22dce241d
efiewura2218 <patrickantwiboasiako22182@gmail.com>
2023-12-18 14:30:20 +0000

    practices hello world

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - this is a function for hello world
+ */
+int main(void)
+{
+puts("Hello, World"); /* puts () automatically appends a newline to the output */
+return (0);
+}
